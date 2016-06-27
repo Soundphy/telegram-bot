c0e0395d1916e821a2bcf9b65d41ae71cd46d453
Clara C <clara.casas.castedo@gmail.com>
2016-06-27 21:31:46 +0200

    Add default sounds and performer field

--- core.py
+++ core.py
@@ -24,20 +24,41 @@ def helpbot(bot, update):
 
 def inlinequery(bot, update):
     query = update.inline_query.query.strip()
-    if not query:
-        return
-    response = requests.get(
-        'https://soundphy-peque.rhcloud.com/v0/search/' + query)
-    data = response.json()['results']
-    # Make sure all IDs are unique
-    data = list(dict((x['identifier'], x) for x in data).values())
-    results = [InlineQueryResultAudio(id=item['identifier'],
-                                      audio_url=item['url'],
-                                      title=item['title'].rstrip('\\'))
-               for item in data]
+    if not query or len(query) < 2:
+        results = [
+            InlineQueryResultAudio(id="eadb1b7155669cee75207fc5065ab4a4389ef3d4",
+                                      audio_url='http://www.instantsfun.es/audio/applause.mp3',
+                                      performer=None,
+                                      title='Applause'),
+            InlineQueryResultAudio(id="2d0dd9f34f0201ebeb9426cf32f768cfa20e4c99",
+                                      audio_url="http://www.instantsfun.es/audio/star_wars_noooo.mp3",
+                                      performer='Darth Vader. Star wars',
+                                      title='Noooo'),
+            InlineQueryResultAudio(id="8ac748e7a6f92c197e5c1663fc0efa8dceb09f7a",
+                                      audio_url="http://www.instantsfun.es/audio/challenge_accepted.mp3",
+                                      performer='Barney. How I met your mother',
+                                      title= "Challenge accepted"),
+            InlineQueryResultAudio(id='f3a1589b686014a6edf79626c8ce3ecc36d70caf',
+                                      audio_url= 'http://www.instantsfun.es/audio/ha_ha.mp3',
+                                      performer='Nelson. The Simpsons',
+                                      title='Ha ha'),
+            InlineQueryResultAudio(id='eec6e23d7367029c438c633cdab59ea81a17a903',
+                                      audio_url= 'http://www.instantsfun.es/audio/crickets.mp3',
+                                      performer=None,
+                                      title='Crickets'),
+        ]
+    else:
+        response = requests.get(
+            'https://soundphy-peque.rhcloud.com/v0/search/' + query)
+        data = response.json()['results']
+        # Make sure all IDs are unique
+        data = list(dict((x['identifier'], x) for x in data).values())
+        results = [InlineQueryResultAudio(id=item['identifier'],
+                                          audio_url=item['url'],
+                                          title=item['title'].rstrip('\\'))
+                   for item in data]
     bot.answerInlineQuery(update.inline_query.id, results=results,
                           cache_time=0)
-
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
